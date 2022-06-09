ca29244acfcf97b1a74e24b0d7adf8e6721909c3
AramLR <aramluck2007@gmail.com>
2022-06-08 20:40:46 -0500

    fix: add validation

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,16 @@
  * 
  * @param byte byte to convert
  * @returns the byte converted to decimal
+ * @throws Error if the byte parameter does not match with /^(0|1)$/
  */
 function byte2Dec(byte:string):bigint{
+
+  const match = /^(0|1)*$/
+
+  if(!match.test(byte)) throw new Error("Invalid byte")
+
   let length = byte.length - 1
-  let res = BigInt(0) 
+  let res = BigInt(0)
 
   for (const bit of byte) {
     res += BigInt(bit) * BigInt(Math.pow(2, length))
